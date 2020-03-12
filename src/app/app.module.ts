159f38f806d2b775569e82c429475b56132a2124
Harry Dbrandy <devdbrandy@gmail.com>
2020-03-12 01:58:53 +0100

    ✨ implement prototype design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ModelContentComponent } from './components/model-content/model-content.component';
+import { ModelImageComponent } from './components/model-image/model-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelContentComponent,
+    ModelImageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/model-image/model-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-model-image',
+  templateUrl: './model-image.component.html',
+  styleUrls: ['./model-image.component.scss']
+})
+export class ModelImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
