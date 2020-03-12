71ea584f611e30611860b9e5c98f9a3cd7bd95a3
Harry Dbrandy <devdbrandy@gmail.com>
2020-03-12 16:12:52 +0100

    🔧 fix misplaced asset paths

--- src/app/shared/slide.data.ts
+++ src/app/shared/slide.data.ts
@@ -1,22 +1,22 @@
 export const slides = [
   {
     alt: 'I/IV',
-    image: 'assets/img/slide-01.png',
+    image: 'assets/img/slide-01.jpg',
     accent: '#0e1d33'
   },
   {
     alt: 'II/IV',
-    image: 'assets/img/slide-02.png',
+    image: 'assets/img/slide-02.jpg',
     accent: '#543000'
   },
   {
     alt: 'III/IV',
-    image: 'assets/img/slide-03.png',
+    image: 'assets/img/slide-03.jpg',
     accent: '#420422'
   },
   {
     alt: 'IV/IV',
-    image: 'assets/img/slide-04.png',
+    image: 'assets/img/slide-04.jpg',
     accent: '#230a22'
   }
 ];
