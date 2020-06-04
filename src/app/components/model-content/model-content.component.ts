0d65d6509575b5b719f768ae2f812f7d90a873a1
Harry Dbrandy <devdbrandy@gmail.com>
2020-06-04 07:43:47 +0100

    feat(slide): add automatice slide control ✨🎉

--- src/app/components/model-content/model-content.component.ts
+++ src/app/components/model-content/model-content.component.ts
@@ -5,13 +5,14 @@ import { SlidesService } from '../../shared/services/slides.service';
 @Component({
   selector: 'app-model-content',
   templateUrl: './model-content.component.html',
-  styleUrls: ['./model-content.component.scss']
+  styleUrls: ['./model-content.component.scss'],
 })
 export class ModelContentComponent implements OnInit {
   title = 'Balinda Christelle';
   role = 'model';
   slides: Slide[];
   currentIndex = 0;
+  timer: any;
 
   constructor(private slideService: SlidesService) {}
 
@@ -20,7 +21,11 @@ export class ModelContentComponent implements OnInit {
   }
 
   loadSlides(): void {
-    this.slideService.getSlides().subscribe(slides => (this.slides = slides));
+    this.slideService.getSlides().subscribe((slides) => (this.slides = slides));
+
+    this.timer = setInterval(() => {
+      this.plusSlides();
+    }, 4000);
   }
 
   showSlide(index) {
@@ -30,6 +35,26 @@ export class ModelContentComponent implements OnInit {
 
   isActiveSlide(slide): boolean {
     return slide === this.slides[this.currentIndex];
+  }
+
+  plusSlides() {
+    this.currentIndex += 1;
+
+    if (this.currentIndex >= this.slides.length) {
+      this.currentIndex = 0;
+    }
+    this.showSlide(this.currentIndex);
+  }
+
+  pause() {
+    clearInterval(this.timer);
+  }
+
+  resume() {
+    clearInterval(this.timer);
+    this.timer = setInterval(() => {
+      this.plusSlides();
+    }, 4000);
   }
 
   get activeSlide(): Slide {
